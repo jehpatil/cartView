1a7f9be793228bad920da62a44df46a0c877b546
jehpatil <jayesh.patil9334@gmail.com>
2016-08-04 17:23:47 +0530

    second commit

--- static/js/cartModel.js
+++ static/js/cartModel.js
@@ -1,36 +1,49 @@
-angular.module("myApp", [])
-	   .controller('myCtrl',function ($scope) {
+var cart = angular.module("myApp", []);
+
+	cart.controller('myCtrl',function ($scope) {
 
 	   	$scope.item_list = [
-	 		  	{	name: "Item_1", price:100, vat: 5,description: "Item_1", quantity:0, addedToCart: false},
- 	           	{   name: "Item_2", price:500, vat: 7,description: "Item_2", quantity:0, addedToCart: false},
- 			   	];
+	 		  	{ id:1,	name: "Item_1", price:100, vat: 5,description: "Item_1", quantity:0, addedToCart: false},
+ 	           	{ id:2,  name: "Item_2", price:500, vat: 7,description: "Item_2", quantity:0, addedToCart: false},
+ 	    	  	{ id:3,	name: "Item_3", price:800, vat: 4,description: "Item_3", quantity:0, addedToCart: false},
+ 	           	{ id:4,  name: "Item_4", price:1000, vat: 6,description: "Item_4", quantity:0, addedToCart: false}
+ 		   	];
 	   	
 	   	$scope.cart_list = [];
 	  
 	   	$scope.addItem = function(item) {
-		    $scope.cart_list.push(item);
-		    item.addedToCart = true;
-	     //    var tot = 0;
-	     //    $scope.tot =(item.price + ((item.vat * item.price) / 100));
-	    	// console.log(item.price, tot);
-	   	}
-	    $scope.deleteItem = function (index) {
-	 	   $scope.cart_list.splice(index, 1);
+	   		if (item.quantity == 0) 
+	   		{
+		    	$scope.cart_list.push(item);
+		    	//$scope.price = item.price * item.quantity;
+		    	item.quantity++;
+		    	console.log($scope.price,item.quantity);
+			}
+			else
+			{
+//		    	$scope.price+=item.price;
+		    	item.quantity++;
+		    	console.log($scope.price,item.quantity);
+			}
 		}
-		$scope.increaseQuantity = function(item) {
-    		item.quantity++;
-  		}
-	  	$scope.decreaseQuantity = function(item) {
-   		item.quantity--;        	
-    	if (item.quantity <= 0) 
-    	{
-            item.quantity = 0;
-     		var itemIndex = $scope.cart_list.indexOf(item);
-            if (itemIndex > -1) {
- 				$scope.cart_list.splice(itemIndex, 1);	
- 	        }
-// 	        console.log(item.quantity, itemIndex);
-        }
-    }
-});
+		$scope.deletecCartItem = function(item) {
+	   		item.quantity--;        	
+	    	if (item.quantity <= 0) 
+	    	{
+	            item.quantity = 0;
+	     		var itemIndex = $scope.cart_list.indexOf(item);
+	            if (itemIndex > -1) {
+	 				$scope.cart_list.splice(itemIndex, 1);	
+	 	        }   
+	// 	        console.log(item.quantity, itemIndex);
+	        }
+    	}
+	    $scope.newTotal = function (item) {
+	    		var cartTotal = 0;
+	   		    $scope.cart_list.forEach(function(item) {
+		    	cartTotal += item.price * item.quantity;
+    				});	
+		    	$scope.cartTotal = cartTotal;
+	//	    	console.log(cartTotal);
+      	}	  	
+ });
